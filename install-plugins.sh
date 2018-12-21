450ccadb1e85c5bf69f088c9d641044531b1d951
Nick <nearwood@gmail.com>
2018-12-20 22:12:37 -0500

    make plugins a script

---
+++ install-plugins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ~/.vim/bundle
+
+#vim-airline
+git clone https://github.com/vim-airline/vim-airline
+
+#vim-gitgutter
+git clone git://github.com/airblade/vim-gitgutter
+
+#vim-jsx
+git clone https://github.com/mxw/vim-jsx.git
+
